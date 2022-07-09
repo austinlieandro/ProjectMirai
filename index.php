ce799819b88dafab4e7c0b210f4a3cf8ad6a4929
Austin Lieandro <s160420079@student.ubaya.ac.id>
2022-07-09 23:38:36 +0700

    Perubahan1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PT.Mirai Mesin Industri</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+</head>
+<body>
+    <header id="header" >
+        <nav class="navbar">
+        <!-- <img src="assets/picture/logoMirai.png" class="logo" alt="Logo Mirai"> -->
+        <a href="index.php"><img src="assets/picture/logoMirai.png" alt="" id="logo" class="img-fluid"></a>
+        <ul>
+            <li><a class="nav-link scrollto active" href="#home">Home</a></li>
+            <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+            <li><a class="nav-link scrollto" href="#product">Product</a></li>
+            <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
+        </ul>
+        </nav>
+    </header>
+</body>
+</html>
