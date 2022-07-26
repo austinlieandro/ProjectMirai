c46275ef258e18694e49e885d70a1ce36fb28f56
Austin Lieandro <s160420079@student.ubaya.ac.id>
2022-07-27 03:06:58 +0700

    27 July 2022

--- product.php
+++ product.php
@@ -5,17 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PT.Mirai Mesin Industri</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-        
-        *{
-            font-family: 'Poppins', sans-serif;
-        }
-
-        
-    </style>
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>product</h1>
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+        <div>
+        <a href="index.php"><img src="assets/picture/logoMirai.png" alt="" id="logo" class="img-fluid"></a>
+        </div>
+
+        <nav class="navbar">
+        <ul>
+            <li><a class="nav-link scrollto" href="index.php">Home</a></li>
+            <li><a class="nav-link scrollto" href="about.php">About Us</a></li>
+            <li><a class="nav-link scrollto active" href="product.php">Product</a></li>
+            <li><a class="nav-link scrollto" href="faq.php">FAQ</a></li>
+        </ul>
+        </nav>
+    </div>
+</header>
 </body>
 </html>
