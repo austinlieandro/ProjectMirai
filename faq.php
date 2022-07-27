ddca71b392abe322b27626fea951dd7eacbe554d
Austin Lieandro <s160420079@student.ubaya.ac.id>
2022-07-27 18:28:55 +0700

    27 July

--- faq.php
+++ faq.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PT.Mirai Mesin Industri</title>
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/stylee.css" rel="stylesheet">
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -31,5 +31,34 @@
         </nav>
     </div>
 </header>
+<img src="assets/picture/putih.jpg" alt="">
+
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 footer-contact">
+                    <h3>PT. Mirai Mesin Industri</h3>
+                    <p>
+                        A108 Adam Street <br>
+                        Surabaya, NY 535022<br>
+                        Indonesia <br><br>
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <br><br>
+                    <p>
+                        <strong>Phone:</strong> +62 82291559706<br>
+                        <strong>Email:</strong> info@example.com<br>
+                    </p>
+                </div>
+                <div class="col-lg-5 col-md-6 mt-2">
+                    <iframe class="map-box" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1663.8375504123223!2d112.76730277048476!3d-7.320733414711787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMTknMTUuMSJTIDExMsKwNDYnMDIuOSJF!5e0!3m2!1sen!2sid!4v1475753468457" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
